9dfd6f0f5fd4d86a0a11d7cca65ddbd81f9b0242
sgj <gjsun255@163.com>
2026-04-03 17:32:13 +0800

    利用多个api获取IP地址，多线程版本

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-def main():
-    print("Hello from what-is-myip!")
+import httpx
+import threading
 
+def get_ip_by_httpbin(client: httpx.Client):
+    """通过 httpbin.org 提供的接口获取 IP address"""
+    url = httpx.URL("https://httpbin.org/ip")
+    r = client.get(url)
+    print(f'HTTPBin: {r.json().get("origin")}')
+
+def get_ip_by_whatismyip(client: httpx.Client):
+    """通过 whatismyip.com 提供的接口获取 IP address"""
+    url = httpx.URL("https://api.whatismyip.com/hp.php")
+
+    # headers 是必要的
+    headers = {
+        "Host": "api.whatismyip.com",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:149.0) Gecko/20100101 Firefox/149.0",
+        "Accept": "*/*",
+        "Accept-Language": "zh-CN,en-US;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "Referer": "https://www.whatismyip.com/",
+        "Origin": "https://www.whatismyip.com",
+        "Connection": "keep-alive",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+        "Priority": "u=4",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache",
+    }
+    r = client.post(url, headers=headers)
+    print(f'WhatISMyIP: {r.json().get("ip")}')
+
+def main():
+    with httpx.Client() as client:
+        t1 = threading.Thread(target=get_ip_by_httpbin, args=(client,))
+        t2 = threading.Thread(target=get_ip_by_whatismyip, args=(client,))
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
 
 if __name__ == "__main__":
     main()
