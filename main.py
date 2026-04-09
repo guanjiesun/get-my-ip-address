8ba61d52505e35f31d668d5a7351f3243b5ee2c2
sgj <gjsun255@163.com>
2026-04-09 18:12:59 +0800

    添加 LENGTH 常量以美化输出

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import httpx
 import threading
 from lxml import html
 
+LENGTH: int = 10  # LENGTH = max(len(authority) for authority in ['IPIN', 'HTTPBIN', 'WHATISMYIP', 'IPCN'])
+
 def get_ip_by_ipcn(client: httpx.Client) -> None:
     """通过 ip.cn 提供的接口获取 IP address"""
     url = httpx.URL("https://ip.cn")
@@ -34,19 +36,19 @@ def get_ip_by_ipcn(client: httpx.Client) -> None:
     # 使用 ticket 获取 IP address
     url = httpx.URL(f"https://my.ip.cn/json/?ticket={ticket}")
     r = client.get(url)
-    print(f'IPCN: {r.json().get("data").get("ip")}', flush=True)
+    print(f'{"IPCN":<{LENGTH}}: {r.json().get("data").get("ip")}', flush=True)
 
 def get_ip_by_ipin(client: httpx.Client) -> None:
     """通过 ipin.io 提供的接口获取 IP address"""
     url = httpx.URL("https://ipin.io/_inquiry/v2/get_client_ip")
     r = client.get(url)
-    print(f'IPIN: {r.json().get("ip")}', flush=True)
+    print(f'{"IPIN":<{LENGTH}}: {r.json().get("ip")}', flush=True)
 
 def get_ip_by_httpbin(client: httpx.Client) -> None:
     """通过 httpbin.org 提供的接口获取 IP address"""
     url = httpx.URL("https://httpbin.org/ip")
     r = client.get(url)
-    print(f'HTTPBin: {r.json().get("origin")}', flush=True)
+    print(f'{"HTTPBIN":<{LENGTH}}: {r.json().get("origin")}', flush=True)
 
 def get_ip_by_whatismyip(client: httpx.Client) -> None:
     """通过 whatismyip.com 提供的接口获取 IP address"""
@@ -70,7 +72,7 @@ def get_ip_by_whatismyip(client: httpx.Client) -> None:
         "Cache-Control": "no-cache",
     }
     r = client.post(url, headers=headers)
-    print(f'WhatISMyIP: {r.json().get("ip")}', flush=True)
+    print(f'{"WhatISMyIP":<{LENGTH}}: {r.json().get("ip")}', flush=True)
 
 def main():
     with httpx.Client() as client:
