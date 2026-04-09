db10ab48efffde2701b4fc4174296b3223648aef
sgj <gjsun255@163.com>
2026-04-09 18:26:44 +0800

    remove useless headers for ipcn api

--- main.py
+++ main.py
@@ -10,21 +10,7 @@ def get_ip_by_ipcn(client: httpx.Client) -> None:
 
     # headers 是必要的（copyed from Chrome DevTools）
     headers = {
-        "Host": "ip.cn",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Cache-Control": "max-age=0",
-        "Connection": "keep-alive",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-User": "?1",
-        "Upgrade-Insecure-Requests": "1",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
-        "sec-ch-ua": '"Chromium";v="146", "Not-A.Brand";v="24", "Google Chrome";v="146"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
     }
 
     # 向请求发送 GET 请求，并解析响应中的 HTML 来提取 ticket
